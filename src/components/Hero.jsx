eefb6710c2515af92f02ecd8d472ccca5ff74fae
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-12 23:02:33 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            Global Talen <br className="sm:block hidden" />{" "}
+            Global Talent <br className="sm:block hidden" />{" "}
             <span className="text-gradient">at your doorstep</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
@@ -25,7 +25,6 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
